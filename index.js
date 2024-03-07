7b999340e4bbc8946009df4df5f510eb36cd874a
Baran Karcı <barankarcim@gmail.com>
2024-03-07 18:09:30 +0300

    firs commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const userObject = {
+    "baran": {
+        name: "baran",
+        surname: "karcı",
+        age: 19
+    },
+    "mustafa": {
+        name: "musti",
+        surname: "tokmak",
+        age: 33
+    }
+}
+
+console.log(userObjectgi )
